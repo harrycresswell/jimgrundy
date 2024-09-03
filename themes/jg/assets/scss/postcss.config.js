fa683ee838862ce85b494bfb68dc053ac978db39
Harry Cresswell <studio@harrycresswell.com>
2024-09-03 17:57:57 +0100

    fix: concatenate media type error

--- themes/jg/assets/scss/postcss.config.js
+++ themes/jg/assets/scss/postcss.config.js
@@ -5,7 +5,7 @@ module.exports = {
     require('postcss-import')({
       path: [themeDir]
     }),
-    require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
+    require('tailwindcss')(themeDir + 'assets/scss/tailwind.config.js'),
     require('autoprefixer')({
       path: [themeDir]
     }),
